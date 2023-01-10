5ef49ae5522b3c5b2bb524d45bde31859c371044
francisco kovacevich <fran.kovacevich@gmail.com>
2023-01-10 20:44:34 +0100

    Fix imports

--- aleph_core/__init__.py
+++ aleph_core/__init__.py
@@ -1,6 +1,8 @@
+from aleph_core.utils.exceptions import Exceptions
+from aleph_core.utils.exceptions import Error
+
 from aleph_core.connections.connection import Connection
 from aleph_core.services.service import Service
 
-from aleph_core.utils.exceptions import Exceptions, Error
 from aleph_core.utils.data import Model
 from aleph_core.utils.data import DataSet
